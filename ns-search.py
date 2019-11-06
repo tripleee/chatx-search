e718a08d3fdc9d32619d2906a8495338a528b317
tripleee <tripleee@users.noreply.github.com>
2019-11-06 13:14:49 +0200

    ns-search.py: properly handle <div>...<br>...</div>

--- ns-search.py
+++ ns-search.py
@@ -10,17 +10,26 @@ def nssearch(ns):
     nspairs = dict()
     nsset = dict()
     for entry in search(ns):
-        extracted = unescape(entry.summary_detail.value)
-        if ": ns ['" not in extracted:
-            continue
-        partition = extracted.split(": ns ['")
-        domain = partition[0].split(": ")[-1]
-        nses = sorted(partition[1].strip("']").split("', '"))
-        nskey = ';'.join(nses)
-        if nskey not in nspairs:
-            nspairs[nskey] = defaultdict(int)
-            nsset[nskey] = nses
-        nspairs[nskey][domain] += 1
+        result = unescape(entry.summary_detail.value)
+        if '<br>' in result:  # or '<br/>' in result:
+            if result.startswith("<div class='full'>"):
+                result = result[18:]
+            if result.endswith("</div>"):
+                result = result[:-6]
+            extracted_lines = [x.strip() for x in result.split('<br>')]
+        else:
+            extracted_lines = [result]
+        for extracted in extracted_lines:
+            if ": ns ['" not in extracted:
+                continue
+            partition = extracted.split(": ns ['")
+            domain = partition[0].split(": ")[-1]
+            nses = sorted(partition[1].strip("']").split("', '"))
+            nskey = ';'.join(nses)
+            if nskey not in nspairs:
+                nspairs[nskey] = defaultdict(int)
+                nsset[nskey] = nses
+            nspairs[nskey][domain] += 1
     for nskey in nsset:
         yield nsset[nskey], nspairs[nskey]
 
